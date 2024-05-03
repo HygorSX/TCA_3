ab6f1aafb44ebad5efce91b2f622958fd39797f1
HygorSX <116827177+HygorSX@users.noreply.github.com>
2024-05-03 00:41:00 +0000

    add_pages

--- app/routes/router.js
+++ app/routes/router.js
@@ -13,7 +13,29 @@ router.get("/buypage", function (req, res) {
     res.render("pages/buypage",{pagina:"pages/buypage"});
 });
 
+router.get("/shape", function (req, res) {
+    res.render("pages/shape",{pagina:"pages/shape"});
+});
 
+router.get("/skate", function (req, res) {
+    res.render("pages/skate",{pagina:"pages/skate"});
+});
+
+router.get("/roupas", function (req, res) {
+    res.render("pages/roupas",{pagina:"pages/roupas"});
+});
+
+router.get("/acessorios", function (req, res) {
+    res.render("pages/acessorios",{pagina:"pages/acessorios"});
+});
+
+router.get("/tenis", function (req, res) {
+    res.render("pages/tenis",{pagina:"pages/tenis"});
+});
+
+router.get("/buypagev2", function (req, res) {
+    res.render("pages/buypagev2",{pagina:"pages/buypagev2"});
+});
 
 
 module.exports = router;
